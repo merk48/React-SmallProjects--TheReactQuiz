6bf75170c7c189010d3d5a0d8a0f13e8c0d5b332
Merk48 <meryKassis48@gmail.com>
2025-07-18 00:27:40 +0300

    init: add initials

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
